16a22b554addd2482051134cc47704d461738bb9
candra1525 <candra_2007@mhs.mdp.ac.id>
2024-11-28 00:19:10 +0700

    Coba Login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response as Response;
 
 class UserController extends Controller
@@ -27,11 +27,6 @@ class UserController extends Controller
                 'error' => $e->getMessage()
             ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
-    }
-
-    public function getAllData()
-    {
-        return User::all();
     }
 
     /**
@@ -68,6 +63,61 @@ class UserController extends Controller
 
     public function login(Request $request)
     {
-        //
+        try {
+            $validator = Validator::make($request->all(), [
+                'no_handphone' => 'required|string',
+                'password' => 'required|string'
+            ]);
+    
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => 'failed',
+                    'message' => 'Gagal melakukan validasi tipe data user',
+                    'errors' => $validator->errors()
+                ], Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+    
+            $validated = $validator->validated();
+            $username = $validated['no_handphone'];
+            $password = $validated['password'];
+          
+            $user = User::where('no_handphone', $username)->first();
+            
+            if (!$user) {
+                return response()->json([
+                    'status' => 'failed',
+                    'message' => 'User not found'
+                ], Response::HTTP_NOT_FOUND);
+            }
+
+            if ($user->deleted_at !== null) {
+                return response()->json([
+                    'status' => 'failed',
+                    'message' => 'Akun telah dihapus, tidak bisa login'
+                ], Response::HTTP_UNAUTHORIZED);
+            }
+    
+    
+            if (password_verify($password, $user->password)) {
+                $token = $user->createToken('access_token')->plainTextToken;
+                $user->token = $token;  
+    
+                return response()->json([
+                    'status' => 'success',
+                    'message' => 'Login success',
+                    'data' => $user,
+                ], Response::HTTP_OK);
+            } else {
+                return response()->json([
+                    'status' => 'failed',
+                    'message' => 'Username atau password salah'
+                ], Response::HTTP_UNAUTHORIZED);
+            }
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'failed',
+                'message' => $e->getMessage()
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
