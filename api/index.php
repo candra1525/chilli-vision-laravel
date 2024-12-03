b3877e68971344f0a48ed2d8b5524e0e64425059
candra1525 <candra_2007@mhs.mdp.ac.id>
2024-12-03 15:23:35 +0700

    Perubahan pada 3 file vercel

--- api/index.php
+++ api/index.php
@@ -1,6 +1,7 @@
 <?php
 
 require __DIR__ . '/../public/index.php';
+require __DIR__ . '/../routes/api.php';
 // require __DIR__ . '/../vendor/autoload.php';
 // $app = require_once __DIR__ . '/../bootstrap/app.php';
 
